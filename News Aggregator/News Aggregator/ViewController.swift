4fd4e54622b43debab387b4ff4097e41e721fcbd
ingwarrrr <igorek0706@gmail.com>
2023-06-27 14:27:22 +0300

    initial commit

---
+++ News Aggregator/News Aggregator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News Aggregator
+//
+//  Created by Igor on 27.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
